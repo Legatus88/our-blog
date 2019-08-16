d5da4a799f3817f99bd173aa1d560a59c1ef94b0
Legatus88 <y1wkn8@gmail.com>
2019-08-16 17:15:18 +0400

    add signup users from ui

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to articles_path, flash: { success: "Welcome to Our Blog, #{@user.username}."} }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
